b5b4c8bf5033bf7c755846456460c5dbe78a2625
Abdelghani <143917624+babdellghani@users.noreply.github.com>
2024-01-02 23:52:45 +0100

    Update check.php

--- check.php
+++ check.php
@@ -1,1 +1,14 @@
-
+<?php
+require_once 'connect.php';
+if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $state = $PDO->prepare('UPDATE todos SET completed = ? WHERE id = ?');
+    if (isset($_POST['completed'])) {
+        $state->execute([1, $id]);
+    } else {
+        $state->execute([0, $id]);
+    }
+    header('Location: index.php');
+    exit();
+}
+?>
