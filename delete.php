34302b52a8151bedcf5fe1cb009412e4ceeda1ed
Abdelghani <143917624+babdellghani@users.noreply.github.com>
2024-01-02 23:53:31 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,12 @@
+<?php
 
+    require_once 'connect.php';
+    $id = $_GET['id'];
+    $state = $PDO->prepare('DELETE FROM todos WHERE id = ?');
+    $delete = $state->execute([$id]);
+    if ($delete) {
+        header('Location: index.php');
+        exit();
+    } else {
+        echo 'Error, Not Deleted';
+    }
